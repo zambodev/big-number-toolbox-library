55673840ecf0fb45c536e0777b1f5878b27ea6d8
Zambo-dev <thomasz01@outlook.it>
2022-08-31 11:11:54 +0200

    Header update: remoed deprecated functions

--- src/include/bntl.h
+++ src/include/bntl.h
@@ -33,8 +33,8 @@
 
 typedef unsigned long ulong;
 typedef char byte;
-typedef unsigned char ubyte;		/* Use byte ad an unsigned char (8 bit) */
-typedef struct BN_T					/* Big number struct */
+typedef unsigned char ubyte;
+typedef struct BN_T
 {
 	size_t size;
 	ulong* num;
@@ -57,10 +57,10 @@ size_t bn_init_s(bn_t *number, char *string);
 ################################
 */
 
-void bn_sum(bn_t *number1, bn_t *number2);			/* Add number2 into number1 */
-void bn_sub(bn_t *number1, bn_t *number2);			/* Subtract number2 from number1 */
-void bn_mul(bn_t *number1, bn_t *number2);			/* Multiply number1 by number2 */
-void bn_div(bn_t *number1, bn_t *number2, bn_t *rest);		/* Divide number1 by number2 and store the rest */
+void bn_sum(bn_t *number1, bn_t *number2);						/* Add number2 into number1 */
+void bn_sub(bn_t *number1, bn_t *number2);						/* Subtract number2 from number1 */
+void bn_mul(bn_t *number1, bn_t *number2);						/* Multiply number1 by number2 */
+void bn_div(bn_t *number1, bn_t *number2, bn_t *rest);			/* Divide number1 by number2 and store the rest */
 
 
 /*
@@ -69,19 +69,17 @@ void bn_div(bn_t *number1, bn_t *number2, bn_t *rest);		/* Divide number1 by num
 ################################
 */
 				
-void bn_inc(bn_t *number);					/* Increment number by one */
-void bn_dec(bn_t *number);					/* Decrement number by one */
-void bn_ext(bn_t *number, size_t bytes);				/* Extend number size by n bytes */
-void bn_free(bn_t *number);					/* Free number */
-void bn_sl(bn_t *number, ulong val);					/* Shift number left by one bit */
-void bn_ssl(bn_t *output, bn_t *number);				/* Shift number left by one bit and store the overflow in output */
-void bn_sr(bn_t *number, ulong val);					/* Shift number right by one bit */
-void bn_ssr(bn_t *output, bn_t *number);				/* Shift number right by one bit and store the overflow in output */
-void bn_srk(bn_t *number);					/* Shrink empty bytes from the front of the number */
-void bn_cpy(bn_t *result, bn_t *number);				/* Copy number into result reallocating the size */
-void bn_hcpy(bn_t *result, bn_t *number);				/* Copy number into result without reallocating (set to 0 the surplus) */
-void bn_ncpy(bn_t *result, bn_t *number, size_t size);		/* Copy n bytes from number to result starting from the less valuable byte */
-void bn_rev(bn_t *number);					/* Revers byte order */
+void bn_inc(bn_t *number);										/* Increment number by one */
+void bn_dec(bn_t *number);										/* Decrement number by one */
+void bn_ext(bn_t *number, size_t bytes);						/* Extend number size by n bytes */
+void bn_free(bn_t *number);										/* Free number */
+void bn_sl(bn_t *number, ulong val);							/* Shift number left by one bit */
+void bn_sr(bn_t *number, ulong val);							/* Shift number right by one bit */
+void bn_srk(bn_t *number);										/* Shrink empty bytes from the front of the number */
+void bn_cpy(bn_t *result, bn_t *number);						/* Copy number into result reallocating the size */
+void bn_hcpy(bn_t *result, bn_t *number);						/* Copy number into result without reallocating (set to 0 the surplus) */
+void bn_ncpy(bn_t *result, bn_t *number, size_t size);			/* Copy n bytes from number to result starting from the less valuable byte */
+void bn_rev(bn_t *number);										/* Revers byte order */
 void bn_comp(bn_t *number1, bn_t *number2, ubyte *result);		/* Compare numbers and set check to 0: equal, 1: first bigger, 2: second bigger */
 
 /*
@@ -90,7 +88,7 @@ void bn_comp(bn_t *number1, bn_t *number2, ubyte *result);		/* Compare numbers a
 ################################
 */
 
-char * bntobs(bn_t *number);					/* Convert number to binary string */
+char * bntobs(bn_t *number);									/* Convert number to binary string */
 
 
 /*
@@ -99,7 +97,7 @@ char * bntobs(bn_t *number);					/* Convert number to binary string */
 ################################
 */
 
-void bn_print(bn_t *number);					/* Print number into stdout in binary string format */
+void bn_print(bn_t *number);									/* Print number into stdout in binary string format */
 
 
 #endif
