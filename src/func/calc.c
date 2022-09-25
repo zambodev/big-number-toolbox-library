ffe6299d6abca0729a56d2597915d9781b7ad593
Zambo_dev <thomasz01@outlook.it>
2022-09-25 09:40:16 +0200

    things

--- src/func/calc.c
+++ src/func/calc.c
@@ -22,14 +22,15 @@ void bn_add(bn_t *destn, bn_t *number1, bn_t *number2)
 	byte inc;
 	size_t size, size1, size2;
 
-	/* Extends result if allocated space isn't enough */
-	if(destn->size < number2->size)
-		bn_ext(destn, destn, number2->size - destn->size);
-
 	size = destn->size/sizeof(ulong);
 	size1 = number1->size/sizeof(ulong);
 	size2 = number2->size/sizeof(ulong);
 	shift = sizeof(ulong)*8/2;
+
+	/* Extends result if allocated space isn't enough */
+	tmp = (size1 < size2) ? size2 : size1;
+	if(destn->size < tmp)
+		bn_ext(destn, destn, (tmp - size)*sizeof(ulong));
 
 	#if(defined(__BYTE_ORDER) && __BYTE_ORDER == __LITTLE_ENDIAN)
 		num1 = number1->num;
@@ -93,25 +94,32 @@ void bn_add(bn_t *destn, bn_t *number1, bn_t *number2)
  *	@param [in,out] number1 Initialized number
  *	@param [in] number2 Initialized number
 */
-void bn_sub(bn_t *number1, bn_t *number2)
+void bn_sub(bn_t *destn, bn_t *number1, bn_t *number2)
 {	
-	if(number1->num == NULL || number2->num == NULL) return;
+	if(number1->size == 0 || number2->size == 0) return;
+	if(number1->size < number2->size) return;
 
-	ulong tmp, carry = 1, carry2 = 0, shift, low, high, store, *num1, *num2;
+	ulong tmp, carry = 1, carry2 = 0, shift, low, high, store, *dst, *num1, *num2;
 	byte inc;
-	size_t size1, size2;
+	size_t size, size1, size2;
 
+	size = destn->size/sizeof(ulong);
 	size1 = number1->size/sizeof(ulong);
 	size2 = number2->size/sizeof(ulong);
 	shift = sizeof(ulong)*8/2;
 
-	if(size1 < size2) return;
+	/* Extends result if allocated space isn't enough */
+	tmp = (size1 < size2) ? size2 : size1;
+	if(destn->size < tmp)
+		bn_ext(destn, destn, (tmp - size)*sizeof(ulong));
 
 	#if(defined(__BYTE_ORDER) && __BYTE_ORDER == __LITTLE_ENDIAN)
 		num1 = number1->num;
 		num2 = number2->num;
+		dst = destn->num;
 		inc = 1;
 	#elif(defined(__BYTE_ORDER) && __BYTE_ORDER == __BIG_ENDIAN)
+		dst = destn->num + size - 1;
 		num1 = number1->num + size1 - 1;
 		num2 = number2->num + size2 - 1;
 		inc = -1;
@@ -121,46 +129,6 @@ void bn_sub(bn_t *number1, bn_t *number2)
 
 	low = ULONG_MAX >> shift;
 	high = ULONG_MAX << shift;
-
-	while(size1 > 0 && (size2 > 0 || carry != 0))
-	{
-		store = (size2 > 0) ? ~(*num2) : 0;
-		tmp = (store & low) + carry;
-		carry = (tmp & high) >> shift;
-		store ^= store & low;
-		store |= tmp & low;;	
-
-		tmp = ((store & high) >> shift);
-		tmp += carry;
-		carry = (tmp & high) >> shift;
-
-		tmp <<= shift;
-		store ^= store & high;
-		store |= tmp;
-
-		tmp = (*num1 & low);
-		tmp += (store & low) + carry2;
-		carry2 = (tmp & high) >> shift;
-		*num1 ^= *num1 & low;
-		*num1 |= tmp & low;
-
-		tmp = (*num1 & high) >> shift;
-		tmp += ((store & high) >> shift) + carry2;
-
-		carry2 = (tmp & high) >> shift;
-		tmp <<= shift;
-
-		*num1 ^= *num1 & high;
-		*num1 |= tmp;
-
-		num1 += inc;
-		--size1;
-		if(size2 > 0)
-		{
-			num2 += inc;
-			--size2;
-		}
-	}
 
 }
 
--- src/include/bntl.h
+++ src/include/bntl.h
@@ -58,7 +58,7 @@ void bn_init_s(bn_t *number, char *string);
 */
 
 void bn_add(bn_t *destn, bn_t *number1, bn_t *number2);						/* Add number2 into number1 */
-void bn_sub(bn_t *number1, bn_t *number2);						/* Subtract number2 from number1 */
+void bn_sub(bn_t *destn, bn_t *number1, bn_t *number2);						/* Subtract number2 from number1 */
 void bn_mul(bn_t *number1, bn_t *number2);						/* Multiply number1 by number2 */
 void bn_div(bn_t *number1, bn_t *number2, bn_t *rest);			/* Divide number1 by number2 and store the rest */
 
